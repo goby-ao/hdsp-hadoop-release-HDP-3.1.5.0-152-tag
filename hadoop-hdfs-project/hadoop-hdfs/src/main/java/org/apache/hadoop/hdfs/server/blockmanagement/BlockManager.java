07d7fd4f23263da420c6ebc711b2c635b0c6ad43
Chen Yao <tracyaoer@gmail.com>
2026-04-09 08:54:49 +0800

    Revert "Applying Patch HDFS-14849:Erasure Coding: the internal block is replicated many times when datanode is decommissioning"
    This reverts commit efc5902d4630ca73d8227514b7b3fe8909c11d15.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
@@ -2284,14 +2284,12 @@ public class BlockManager implements BlockStatsMXBean {
         if (isStriped) {
           byte blockIndex = ((BlockInfoStriped) block).
               getStorageBlockIndex(storage);
-          if (state == StoredReplicaState.LIVE) {
-            liveBlockIndices.add(blockIndex);
-            if (!bitSet.get(blockIndex)) {
-              bitSet.set(blockIndex);
-            } else {
-              numReplicas.subtract(StoredReplicaState.LIVE, 1);
-              numReplicas.add(StoredReplicaState.REDUNDANT, 1);
-            }
+          liveBlockIndices.add(blockIndex);
+          if (!bitSet.get(blockIndex)) {
+            bitSet.set(blockIndex);
+          } else if (state == StoredReplicaState.LIVE) {
+            numReplicas.subtract(StoredReplicaState.LIVE, 1);
+            numReplicas.add(StoredReplicaState.REDUNDANT, 1);
           }
         }
         continue;
